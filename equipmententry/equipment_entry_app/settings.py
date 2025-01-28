cae4083485deee493a58367226ccb5f0ddf625a5
Mohamed Mostafa <ibrahim.mohamed@gizasystems.com>
2025-01-28 22:24:25 +0200

    Set up Django project structure with initial configurations and Docker support

--- equipmententry/equipment_entry_app/settings.py
+++ equipmententry/equipment_entry_app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'equipment_entry_app',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'equipmententry.urls'
+ROOT_URLCONF = 'equipment_entry_app.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'equipmententry.wsgi.application'
+WSGI_APPLICATION = 'equipment_entry_app.wsgi.application'
 
 
 # Database
